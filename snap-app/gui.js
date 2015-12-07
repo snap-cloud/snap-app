c785fcfc77019ac9b80a2a54e398ed0924d5e4b6
Michael Ball <cycomachead@gmail.com>
2015-12-07 02:26:31 -0800

    Saving image, temp support

--- snap-app/gui.js
+++ snap-app/gui.js
@@ -5,12 +5,37 @@ IDE_Morph.prototype.saveFileAs = function (
     fileName,
     newWindow
 ) {
+    
+    var blobIsSupported = false,
+        world = this.world(),
+        fileExt,
+        dataURI, dialog;
+
+    // fileType is a <kind>/<ext>;<charset> format.
+    fileExt = fileType.split('/')[1].split(';')[0];
+    // handle text/plain as a .txt file
+    fileExt = '.' + (fileExt === 'plain' ? 'txt' : fileExt);
+
+    // This is a workaround for a known Chrome crash with large URLs
+    function exhibitsChomeBug(contents) {
+        var MAX_LENGTH = 2e6,
+        isChrome  = navigator.userAgent.indexOf('Chrome') !== -1;
+        return isChrome && contents.length > MAX_LENGTH;
+    }
+
     //saveFile(fileName, fileContents);
     var fs = require('fs');
     var dialog = require('electron').dialog;
+    if (fileExt === '.png') {
+        nativeImage = require('electron').nativeImage
+        img = nativeImage.createFromDataURL(contents);
+        contents = img.toPng();
+    }
     // save As Stuff
+    // TODO: Show a dialog, but that doesn't work?
+    // TODO: What to do about encoding?
     
-    fs.writeFile(fileName, contents);
+    fs.writeFile(fileName + fileExt, contents);
     console.log('Saved??');
     
       // dialog.showSaveDialog({ filters: [
